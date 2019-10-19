e8110a0bf43c9f604d18ffcb06eb1429254b0855
Graham Thompson <grwthomps@gmail.com>
2019-10-19 16:55:27 -0600

    Add test for linking instructor names to students index spec

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -26,4 +26,21 @@ RSpec.describe 'Student index page', type: :feature do
       expect(page).to_not have_content(student_1.cohort)
     end
   end
+
+  it 'has links for instructor names' do
+    instructor_1 = Instructor.create!(name: 'Jerry Gergich')
+    instructor_2 = Instructor.create!(name: 'Ann Perkins')
+    student_1 = instructor_1.students.create!(name: 'Andy Dwyer', cohort: 1608, age: 24)
+    student_2 = instructor_2.students.create!(name: 'Leslie Knope', cohort: 1610, age: 28)
+
+    visit '/students'
+
+    within "#student-#{student_1.id}" do
+      expect(page).to have_link(instructor_1.name)
+    end
+
+    within "#student-#{student_2.id}" do
+      expect(page).to have_link(instructor_2.name)
+    end
+  end
 end
